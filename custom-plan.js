520b75e2cff9431e11094af3de4cdb07500dc40a
Leandro Almeida <leanazulyoro@gmail.com>
2019-08-12 14:21:34 -0300

    open custom-plan url in new tab

--- custom-plan.js
+++ custom-plan.js
@@ -1,4 +1,4 @@
 // Redirects to the custom_plans url in overol for the current sellerId in sellers-ui
 var pathParts = window.location.pathname.split('/');
 var sellerId = pathParts[2];
-location.href = `https://www.properati.com.ar.local.coco:19292/panel/commercial/custom_plans/${sellerId}`;
+window.open(`https://www.properati.com.ar.local.coco:19292/panel/commercial/custom_plans/${sellerId}`);
