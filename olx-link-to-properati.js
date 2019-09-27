39cbcf1f1e4b1569be349da73ae5ceb712648e80
Leandro Almeida <leanazulyoro@gmail.com>
2019-09-27 11:30:43 -0300

    feat: replace url of olx items with properati's item url

--- olx-link-to-properati.js
+++ olx-link-to-properati.js
@@ -1,2 +1,14 @@
+const properatiURLs = {
+  1055130280: 'https://www.properati.com.co/19gbx_venta_casa_tulua_patio_balcon_primer-piso_patio-de-ropas_sala-comedor_inmobiliaria-invercol'
+};
+const itemIds = Array.from(
+    document.querySelectorAll('.items-list .item a[data-id]')
+  )
+  .map(item => item.getAttribute('data-id'));
+const filteredItems = itemIds.filter(olxId => Object.keys(properatiURLs).includes(olxId));
+filteredItems.map(id => {
+  document.querySelector(`a[href$="${id}"]`).setAttribute('href', properatiURLs[id]);
+});
+
 
 
