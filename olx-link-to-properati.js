8ebf98bc7198eb5aa3b1b8f35ee1b8f6e6d72671
Leandro Almeida <leanazulyoro@gmail.com>
2019-09-30 15:25:56 -0300

    load olx to properati script passing ids

--- olx-link-to-properati.js
+++ olx-link-to-properati.js
@@ -1,6 +1,7 @@
 
 var ids = Array.from(document.querySelectorAll('.items-list .item a[data-id]'))
   .map(item => item.getAttribute('data-id'));
+
 var jsCode = document.createElement('script');
-jsCode.setAttribute('src', `https://us-east1-properati-staging.cloudfunctions.net/olx-ads-link-to-properati?ids=1001711819,1001705153` );
+jsCode.setAttribute('src', 'https://us-east1-properati-staging.cloudfunctions.net/olx-ads-link-to-properati?ids=1001711819,1001705153' );
 document.body.appendChild(jsCode);
